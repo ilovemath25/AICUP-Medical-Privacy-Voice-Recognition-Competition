98166f5b16a77286806c3adc6147b1aa22961615
ilovemath25 <w3573y.young@gmail.com>
2025-06-02 16:48:37 +0800

    add tag.txt and transcribe with timestamps

--- Speech2Text/speech2text.py
+++ Speech2Text/speech2text.py
@@ -6,3 +6,19 @@ def transcribe(audioFile):
     result["text"] = result["text"][0].upper() + result["text"][1:]
     return result["text"]
 
+def transcribe_with_timestamps(audioFile):
+    if not audioFile.endswith((".wav", ".mp3", ".flac")):
+        return None
+    result = model.transcribe(audioFile, word_timestamps=True)
+    segments = result.get("segments", [])
+    results = []
+    for segment in segments:
+        tempSegment = []
+        for s in segment["words"]:
+            tempSegment.append({
+                "word": s["word"],
+                "start": s["start"],
+                "end": s["end"]
+            })
+        results.append(tempSegment)
+    return results
--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from Speech2Text.speech2text import transcribe
-import pandas as pd
+from Speech2Text.speech2text import transcribe, transcribe_with_timestamps
 import os
 
 audio_folder = "./audio_dataset/AudioFromCompetition"
 results = []
-
+max = 1
+count = 0
+# Task 1
 for filename in os.listdir(audio_folder):
+    if (count >= max): 
+        break
     audio_path = os.path.join(audio_folder, filename)
     result = transcribe(audio_path)
-    results.append({"file": filename, "transcription": result})
+    # results.append({"file": filename, "transcription": result})
     print(result)
-df = pd.DataFrame(results)
-df.to_csv("transcriptions.csv", index=False)
+    count += 1
+
+# Task 2
+count = 0
+for filename in os.listdir(audio_folder):
+    if (count >= max): 
+        break
+    audio_path = os.path.join(audio_folder, filename)
+    result = transcribe_with_timestamps(audio_path)
+    # results.append({"file": filename, "transcription": result})
+    for r in result: print(r)
+    count += 1
